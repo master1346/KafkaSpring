6e041a2072e4f767b68853a7af6eca3181790b3a
master1346 <shedrin.yakudza46@gmail.com>
2022-08-18 17:36:21 +0300

    Commit 0

---
+++ src/main/java/net/javaguides/springbootkafka/controller/MessageController.java
@@ -0,0 +1,22 @@
+package net.javaguides.springbootkafka.controller;
+
+import net.javaguides.springbootkafka.kafka.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+    private final KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to the topic");
+    }
+}
